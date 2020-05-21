b27f306277a4c47ef7e2d2d23503d023621b6aac
DevBara <61251493+DevBara@users.noreply.github.com>
2020-05-21 10:12:34 -0500

    corrected errors for data to render

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    isLoading: true,
+    employees: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('/tomio_api/v1/employees');
+    const body = await response.json();
+    this.setState({ employees: body, isLoading: false });
+  }
+
+  render() {
+    const {employees, isLoading} = this.state;
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className="App-intro">
+            <h2>Employee List</h2>
+            {employees.map(employee =>
+              <div key={employee.id}>
+                {employee.firstName}
+                <br></br>
+                {employee.lastName}
+                <br></br>
+                {employee.email}
+              </div>
+            )}
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
