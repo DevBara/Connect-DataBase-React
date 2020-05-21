941f2bfdb4cbcc9f98bc372c784a190656790b80
DevBara <61251493+DevBara@users.noreply.github.com>
2020-05-21 10:29:53 -0500

    cleaned up since new component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Employee from './components/Employee'
 
-class App extends Component {
-  state = {
-    isLoading: true,
-    employees: []
-  };
 
-  async componentDidMount() {
-    const response = await fetch('/tomio_api/v1/employees');
-    const body = await response.json();
-    this.setState({ employees: body, isLoading: false });
-  }
-
-  render() {
-    const {employees, isLoading} = this.state;
-
-    if (isLoading) {
-      return <p>Loading...</p>;
-    }
+function App (){
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <div className="App-intro">
-            <h2>Employee List</h2>
-            {employees.map(employee =>
-              <div key={employee.id}>
-                {employee.firstName}
-                <br></br>
-                {employee.lastName}
-                <br></br>
-                {employee.email}
-              </div>
-            )}
-          </div>
-        </header>
+        <Employee />
+       
       </div>
-    );
+    )
   }
-}
+
 
 export default App;
