ce25887efead28784701822ee5d878571ca54194
DevBara <61251493+DevBara@users.noreply.github.com>
2020-05-28 12:18:37 -0500

    additional edits

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,53 +1,83 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Button, ButtonGroup, Container, Table } from 'reactstrap';
+import AppNavbar from './AppNavBar';
+import { Link } from 'react-router-dom';
 
-export default class employee extends Component {
-    constructor(props){
-        super(props);
+class EmployeeList extends Component {
 
-        this.state ={
-            isLoading: true,
-            employees: []
-        };
+  constructor(props) {
+    super(props);
+    this.state = {employees: [], isLoading: true};
+    this.remove = this.remove.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({isLoading: true});
+
+    fetch('/tomio_api/v1/employees')
+      .then(response => response.json())
+      .then(data => this.setState({employees: data, isLoading: false}));
+  }
+
+  async remove(id) {
+    await fetch(`/tomio_api/v1/employees/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).then(() => {
+      let updatedEmployees = [...this.state.employees].filter(i => i.id !== id);
+      this.setState({employees: updatedEmployees});
+    });
+  }
+
+  render() {
+    const {employees, isLoading} = this.state;
+
+    if (isLoading) {
+      return <p>Loading...</p>;
     }
 
-        async componentDidMount(){
-            //grab url use for Request Mapping in Eclipse
-            const response = await fetch('/tomio_api/v1/employees');
-            const body = await response.json();
-            this.setState({employees:body, isLoading: false});
-        }
-    
-    render() {
-        const {employees,isLoading} =this.state;
+    const employeeList = employees.map(employee => {
+      return <tr key={employee.id}>
+        <td style={{whiteSpace: 'nowrap'}}>{employee.firstName}</td>
+        <td>{employee.lastName}</td>
+        <td>{employee.email}</td>
+        <td>
+          <ButtonGroup>
+            <Button size="sm" color="primary" tag={Link} to={"/employee/" + employee.id}>Edit</Button>
+            <Button size="sm" color="danger" onClick={() => this.remove(employee.id)}>Delete</Button>
+          </ButtonGroup>
+        </td>
+      </tr>
+    });
 
-        if(isLoading){
-            return<p>Loading...</p>
-        }
-        return (
-            <div className ="employeeContainer">
-                <h2>Employee List</h2>
-                {employees.map(employee =>
-                  <div key={employee.id} className="employeeParent">
-                      <table className="employeeTable">
-                        <tr>
-                          <th>Employee Id</th>
-                          <th>First Name</th>
-                          <th>Last Name</th>
-                          <th>Email</th>
-                        </tr>
-                        <tr>
-                          <td>{employee.id}</td>
-                          <td>{employee.firstName}</td>
-                          <td>{employee.lastName}</td>
-                          <td>{employee.email}</td>
-                        </tr>
-                      </table>
-                  </div>
-                )}
-                <button>Edit</button>
-                <button>Update</button>
-
+    return (
+      <div>
+        <AppNavbar/>
+        <Container fluid>
+          <div className="float-right">
+            <Button color="success" tag={Link} to="/employee/new">Add Group</Button>
           </div>
-        )
-    }
+          <h3>My Employees List</h3>
+          <Table className="mt-4">
+            <thead>
+            <tr>
+              <th width="20%">First Name</th>
+              <th width="20%">Last Name</th>
+              <th>Email</th>
+              <th width="10%">Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+            {employeeList}
+            </tbody>
+          </Table>
+        </Container>
+      </div>
+    );
+  }
 }
+
+export default EmployeeList;
